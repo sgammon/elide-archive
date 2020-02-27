6d6198933b3565da21e2d4db4e4c3ed242754e0c
Sam Gammon <sam@bloombox.io>
2020-02-26 17:15:16 -0800

    Adjust build stamp to fallback to COMMIT_SHA variable

--- tools/bazel_stamp_vars.sh
+++ tools/bazel_stamp_vars.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo BUILD_SCM_REVISION $(git rev-parse --short HEAD)
-echo BUILD_SCM_VERSION $(git describe --abbrev=7 --always --tags HEAD)
-echo BUILD_GUST_VERSION $(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+echo BUILD_SCM_REVISION $(git rev-parse --short HEAD) || $SHORT_SHA
+echo BUILD_SCM_VERSION $(git describe --abbrev=7 --always --tags HEAD) || $SHORT_SHA
+echo BUILD_GUST_VERSION $(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]') || $SHORT_SHA
