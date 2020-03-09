3aa9ddf2c64ea94663fc897a151e0870681d5dc6
Sam Gammon <sam@bloombox.io>
2020-03-09 12:44:00 -0700

    Fix coverage/test result uploads

--- tools/report_java_coverage.sh
+++ tools/report_java_coverage.sh
@@ -3,7 +3,7 @@
 set -o xtrace;
 
 echo "Uploading to Codecov...";
-curl https://codecov.io/bash > ./.codecov.sh && chmod +x ./.codecov.sh && \
+curl -L https://codecov.io/bash > ./.codecov.sh && chmod +x ./.codecov.sh && \
 	./.codecov.sh -f "$1/lcov.dat" \
                 -X gcov -X coveragepy -X search -X xcode -X gcovout -Z \
                 -F $2 \
@@ -11,7 +11,7 @@ curl https://codecov.io/bash > ./.codecov.sh && chmod +x ./.codecov.sh && \
 	rm -f ./.codecov.sh;
 
 echo "Uploading to CodeClimate...";
-curl https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./codeclimate.sh && \
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./codeclimate.sh && \
   chmod +x ./codeclimate.sh && \
   ./codeclimate.sh format-coverage --input-type lcov --output reports/codeclimate.json "$1/lcov.dat" && \
   ./codeclimate.sh upload-coverage --input reports/codeclimate.json;
