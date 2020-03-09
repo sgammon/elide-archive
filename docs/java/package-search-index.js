6d6742d9cd114ef806e5ba74b34baf1ed03ca205
Sam Gammon <sam@bloombox.io>
2020-03-09 12:40:33 -0700

    Drop docs for their own PR

--- docs/java/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"gust"},{"l":"gust.backend"},{"l":"gust.backend.driver.firestore"},{"l":"gust.backend.driver.inmemory"},{"l":"gust.backend.model"},{"l":"gust.backend.runtime"},{"l":"gust.backend.transport"}]
