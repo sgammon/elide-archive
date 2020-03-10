ff827b84d2e38e26338147cfc593959065cd3c51
Sam Gammon <sam@bloombox.io>
2020-03-09 18:03:19 -0700

    Add license header, codebase-wide

--- java/gust/backend/model/ModelWriteConflict.java
+++ java/gust/backend/model/ModelWriteConflict.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright © 2020, The Gust Framework Authors. All rights reserved.
+ *
+ * The Gust/Elide framework and tools, and all associated source or object computer code, except where otherwise noted,
+ * are licensed under the Zero Prosperity license, which is enclosed in this repository, in the file LICENSE.txt. Use of
+ * this code in object or source form requires and implies consent and agreement to that license in principle and
+ * practice. Source or object code not listing this header, or unless specified otherwise, remain the property of
+ * Elide LLC and its suppliers, if any. The intellectual and technical concepts contained herein are proprietary to
+ * Elide LLC and its suppliers and may be covered by U.S. and Foreign Patents, or patents in process, and are protected
+ * by trade secret and copyright law. Dissemination of this information, or reproduction of this material, in any form,
+ * is strictly forbidden except in adherence with assigned license requirements.
+ */
 package gust.backend.model;
 
 import com.google.protobuf.Message;
@@ -6,6 +18,7 @@ import javax.annotation.Nullable;
 
 
 /** Thrown when a write operation fails, because of some conflict situation. */
+@SuppressWarnings("WeakerAccess")
 public final class ModelWriteConflict extends ModelWriteFailure {
   /** Expectation that was violated during the write operation. */
   private final @Nonnull WriteOptions.WriteDisposition failedExpectation;
--- java/gust/backend/model/ObjectModelCodec.java
+++ java/gust/backend/model/ObjectModelCodec.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright © 2020, The Gust Framework Authors. All rights reserved.
+ *
+ * The Gust/Elide framework and tools, and all associated source or object computer code, except where otherwise noted,
+ * are licensed under the Zero Prosperity license, which is enclosed in this repository, in the file LICENSE.txt. Use of
+ * this code in object or source form requires and implies consent and agreement to that license in principle and
+ * practice. Source or object code not listing this header, or unless specified otherwise, remain the property of
+ * Elide LLC and its suppliers, if any. The intellectual and technical concepts contained herein are proprietary to
+ * Elide LLC and its suppliers and may be covered by U.S. and Foreign Patents, or patents in process, and are protected
+ * by trade secret and copyright law. Dissemination of this information, or reproduction of this material, in any form,
+ * is strictly forbidden except in adherence with assigned license requirements.
+ */
 package gust.backend.model;
 
 import com.google.protobuf.Message;
@@ -14,6 +26,7 @@ import java.util.Map;
  */
 @Immutable
 @ThreadSafe
+@SuppressWarnings("WeakerAccess")
 public final class ObjectModelCodec<Model extends Message> implements ModelCodec<Model, Map<String, ?>> {
   /** Model builder instance to use for spawning models. */
   private final Message.Builder builder;
