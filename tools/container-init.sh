594192de2d1e0f8a384efda25fd696bf143ab6d0
Sam Gammon <sam.g@cookiescalifornia.com>
2021-08-22 21:20:53 -0700

    Tweaks for init script safety

--- tools/container-init.sh
+++ tools/container-init.sh
@@ -1,25 +1,26 @@
-#!bash
+#!/bin/bash
 
-export HOME=/home/`whoami`;
+# shellcheck disable=SC2155
+export HOME=/home/$(whoami);
 
 echo "Setting up credentials...";
 ## setup credentials
 if [[ -z "${GOOGLE_CREDENTIALS}" ]]; then
-  mkdir -p $HOME/.config/gcloud;
-  echo "${GOOGLE_CREDENTIALS}" > $HOME/.config/gcloud/application_default_credentials.json;
+  mkdir -p "$HOME/.config/gcloud";
+  echo "${GOOGLE_CREDENTIALS}" > "$HOME/.config/gcloud/application_default_credentials.json";
 fi
 
 if [[ -z "${BUILDBUDDY_CERT}" ]]; then
-  echo "${BUILDBUDDY_CERT}" > $HOME/buildbuddy-cert.pem;
+  echo "${BUILDBUDDY_CERT}" > "$HOME/buildbuddy-cert.pem";
 fi
 
 if [[ -z "${BUILDBUDDY_KEY}" ]]; then
-  echo "${BUILDBUDDY_KEY}" > $HOME/buildbuddy-key.pem;
+  echo "${BUILDBUDDY_KEY}" > "$HOME/buildbuddy-key.pem";
 fi
 
 if [[ -z "${SSHKEY}" ]]; then
-  mkdir -p $HOME/.ssh;
-  echo "${SSHKEY}" > $HOME/.ssh/id_rsa;
-  chown dev:engineering $HOME/.ssh/id_rsa;
-  chmod 600 $HOME/.ssh/id_rsa;
+  mkdir -p "$HOME/.ssh";
+  echo "${SSHKEY}" > "$HOME/.ssh/id_rsa";
+  chown dev:engineering "$HOME/.ssh/id_rsa";
+  chmod 600 "$HOME/.ssh/id_rsa";
 fi
